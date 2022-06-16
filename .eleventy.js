9fce415190ebe2543c051d23e72c4f7b465bcb4c
Tom Peters <tompeters.home@gmail.com>
2022-06-16 22:44:56 +0200

    Added website fonts.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/css");
+
     return {
         dir: {
             input: "src",
