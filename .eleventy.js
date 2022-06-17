08a63ff9fdf9bd72cd939cea6213402b3c026b2f
Tom Peters <tompeters.home@gmail.com>
2022-06-17 02:43:52 +0200

    Added navbar, added stub pages, added CSS, added images.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/css");
     eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addWatchTarget("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/img");
 
     return {
         dir: {
